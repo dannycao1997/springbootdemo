21b26eac5d9f585f96857e93d45b0c4b6f176d32
Danny Cao <dannycao1997@yahoo.com>
2024-07-08 18:28:56 -0400

    added another student

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student danny = new Student(
+                    1L,
+                    "Danny",
+                    "dannycao1997@yahoo.com",
+                    LocalDate.of(1997, Month.SEPTEMBER, 9),
+                    26
+            );
+            Student andy = new Student(
+                    "Andy",
+                    "andy@gmail.com",
+                    LocalDate.of(1997, Month.DECEMBER, 1),
+                    26
+            );
+
+            repository.saveAll(
+                    List.of(danny, andy)
+            );
+        };
+    }
+}
