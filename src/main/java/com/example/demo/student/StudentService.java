429c72520ee0b0fbb8863de55cedd2026dcb0969
Danny Cao <dannycao1997@yahoo.com>
2024-07-08 18:11:54 -0400

    created repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,15 +12,14 @@ import java.util.List;
 @Service // annotation might to be for service class
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) { // contstructor
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of( // CLASS CONVERTED TO JSON FORMAT array localhost8080
-                new Student(
-                        1L,
-                        "Danny",
-                        "dannycao1997@yahoo.com",
-                        LocalDate.of(1997, Month.SEPTEMBER, 9),
-                        26
-                )
-        );
+        return studentRepository.findAll(); // CLASS CONVERTED TO JSON FORMAT array localhost8080
     }
 }
